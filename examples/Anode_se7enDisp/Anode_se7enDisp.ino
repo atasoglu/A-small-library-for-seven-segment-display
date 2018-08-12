f5096e129c1d2b4de96af3305caeb07232054d51
Ahmet Ataşoğlu <ahmetatasoglu98@gmail.com>
2018-08-12 08:59:49 +0300

    Update Anode_se7enDisp.ino

--- examples/Anode_se7enDisp/Anode_se7enDisp.ino
+++ examples/Anode_se7enDisp/Anode_se7enDisp.ino
@@ -1,5 +1,5 @@
 #include <se7enDisp.h>  // kütüphanemiz dahil edildi
-se7enDisp ssd(4,5,6,7,8,9,10);  // pinler burada tanımladır.
+se7enDisp ssd(4,5,6,7,8,9,10);  // pinler burada tanımlanır.
 
 void setup() {
   ssd.setAnode(); // ortak Anot - Seven Segment Display için bu fonksiyon kullanılmalıdır.
@@ -7,10 +7,13 @@ void setup() {
 
 void loop() {
   for (int i=0; i<=2; i++) { // Bu döngü, ekran üzerinde 5 sayısını 0.5 sn aralıklarla 3 kez gösterecek
-    ssd.five(); // ekrana 5 sayısını yazdırdık
-    delay(500); // 0.5 saniye bekletildi
-    ssd.shotdown(); // tüm ledler söndürüldü
-    delay(500);
+    ssd.five();       // ekrana 5 sayısını yazdırdık
+    delay(500);       // 0.5 saniye bekletildi
+    ssd.shotdown();   // tüm ledler söndürüldü
+    delay(500);       // 0.5 saniye bekletildi
   }
-  ssd.timer(5,1000);
+  ssd.timer(5,1000);  // timer() metodu iki parametrelidir.
+                      // 1. parametre: Başlangıç saniyesini belirtir.
+                      // 2. parametre: Bekleme süresini belirtir. (milisaniye)
+                      // bu metod, parametresiz olarak da kullanılabilir.
 }
