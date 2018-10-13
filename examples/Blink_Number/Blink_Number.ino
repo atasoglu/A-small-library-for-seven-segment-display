dc41eac092ea0a4f98c9dab771178c25a380332e
Ahmet Ataşoğlu <ahmetatasoglu98@gmail.com>
2018-10-13 15:33:38 +0300

    Add files via upload

---
+++ examples/Blink_Number/Blink_Number.ino
@@ -0,0 +1,14 @@
+#include <se7enDisp.h>
+se7enDisp ssd(4,5,6,7,8,9,10); // connection pins
+
+void setup() {
+  ssd.setCathode();
+}
+
+void loop() {
+  ssd.blinkNum(5,3);
+  // first parameter : number
+  // second parameter : iteration
+  // third parameter : delaying time (default value: 1000)
+  delay(500);
+}
---
+++ examples/Print_Number/Print_Number.ino
@@ -0,0 +1,18 @@
+#include <se7enDisp.h>
+se7enDisp ssd(4,5,6,7,8,9,10); // connection pins
+int incomingNum;
+
+void setup() {
+  Serial.begin(9600);
+  ssd.setCathode();
+  // ssd.setAnode();
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    incomingNum = Serial.read() - '0'; // byte to decimal conversion via ascii table
+    Serial.println(incomingNum);
+    ssd.printNum(incomingNum);
+    // first parameter : number to print on display
+  }
+}
